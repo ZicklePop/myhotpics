b94b458a12b474b0fe2172fed01d536fd4a91731
Jake Bilbrey <me@jakebilbrey.com>
2020-02-02 11:34:30 -0800

    nit

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ const Index = ({ query, gifs }) => {
       <Search defaultValue={query} />
       {map(gifs, el => (
         <Gif
-          key={el.guid}
+          key={el.title}
           {...el}
         />
       ))}
