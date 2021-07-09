430ac389f91022ad77cd24772e02bebcea18e5d4
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2021-07-08 21:14:38 -0700

    swapping lint, cleanup

--- pages/api/gifs.js
+++ pages/api/gifs.js
@@ -1,7 +1,7 @@
 import listObjects from '../../scripts/list-objects'
 import get from 'lodash/get'
 
-export default async (req, res) => {
+async function gifs (req, res) {
   const query = get(req, 'query.q')
   const images = await listObjects(query)
   const out = {
@@ -9,3 +9,6 @@ export default async (req, res) => {
   }
   res.status(200).json(out)
 }
+
+export default gifs
+
